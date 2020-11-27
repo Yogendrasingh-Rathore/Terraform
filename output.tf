0d44f29c16eb8109700ff825efc9c75c8c90b8eb
Yogendrasingh-rathore <ysrathore1995@gmail.com>
2020-11-27 16:40:33 +0530

    Updated out variables

--- output.tf
+++ output.tf
@@ -1,8 +1,7 @@
 output "ec2_info"{
-# filter
  value = aws_instance.terraform_ec2.public_ip
 }
 
 output "s3_info"{
- value = aws_s3.terraform-bucket.version
+ value = aws_s3_bucket.terraform-bucket.versioning
 }
